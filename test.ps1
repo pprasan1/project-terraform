5e1b8ab9bea2f2b74ed99403f0ea2dfd5a68fe06
pprasan1 <112462094+pprasan1@users.noreply.github.com>
2022-10-20 20:07:36 +0530

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,30 +1,8 @@
 param(
-
-
-
 [String] $p1,
-
-
-
 [String] $p2,
-
-
-
 [String] $p3
-
-
-
-
 )
-
-
-
 New-Item -Path $p1 -Itemtype Directory
-
-
-
 New-Item -Path $p2 -Itemtype Directory
-
-
-
 New-Item -Path $p3 -Itemtype Directory
