5bc826d192303b1b7d3e5053d4506e41029746d9
pprasan1 <112462094+pprasan1@users.noreply.github.com>
2022-10-21 14:31:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,5 +37,14 @@ resource "null_resource" "PowerShellScriptRunFirstTime" {
             "powershell.exe C:\\temp\\test.ps1 -p1 'c:\\temp\\abc' -p2 'c:\\temp\\xyz'"
       ]
   }
+  user_data =  <<EOF
+    <powershell>
+    # Install OpenSSH
+    Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
+    Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+    Start-Service sshd
+    Set-Service -Name sshd -StartupType 'Automatic'
+    </powershell>
+EOF
 
 }
